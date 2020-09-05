121196f52fb4d8631b35e2738392299c4662ffd0
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-09-05 21:00:58 +0200

    add sass partials and variables

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import './App.css';
+import './sass/style.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <>
+    <div className="wrapper">
+      <div className="container">
+          <h1>Oslo Bysykkel</h1>
+          <p>Frogner Plass</p>
+          <p>Skøyen Stasjon</p>
+          <p>7 Juni plassen</p>
+          <p>Sotahjørnet</p>
+          <p>Frogner Plass</p>
+          <p>Skøyen Stasjon</p>
+          <p>7 Juni plassen</p>
+      </div>
     </div>
+    <footer className="footer">
+      <button className="btn btn-primary">Rent this bike</button>
+      <button className="btn btn-secondary">Dock here</button>
+    </footer>
+    </>
   );
 }
 
