f52bd59ec72be78232fdb60af3aa9321bf26da9b
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-09-07 14:09:51 +0200

    create api service

--- assignment2/src/constants/api/OsloBysykkel.js
+++ assignment2/src/constants/api/OsloBysykkel.js
@@ -1,5 +1,26 @@
-export const station_information =
-  "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json";
+import { useState, useEffect } from 'react';
 
-export const station_status =
-  "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json";
+const BASE_URL = "https://gbfs.urbansharing.com/oslobysykkel.no/";
+const STATIONS = "station_information.json";
+const STATUS = "station_status.json";
+const INIT = {
+    method: "GET",
+    cache: "default",
+    mode: "cors",
+    headers: {
+    } 
+}
+
+const useData = (status) => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(BASE_URL + status, INIT)
+        .then(response => response.json())
+        .then(data => setData(data))
+    }, []);
+
+    return data;
+}
+
+export {useData, STATIONS, STATUS}
